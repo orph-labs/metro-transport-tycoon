f4202d579222ff5c03c6acf5bf12a00f0190e54f
angrypig555 <brny@brny.dev>
2026-02-28 17:10:25 +0000

    add level 3

---
+++ train_move_lvl3.gd
@@ -0,0 +1,26 @@
+extends StaticBody2D
+
+var speed = 200
+var do_move_train = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$"../Button2".send_train2.connect(_send_train)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if do_move_train == true:
+		position.x += speed * delta
+	pass
+
+func _send_train():
+		print("sending train")
+		do_move_train = true
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("train entered")
+	do_move_train = false
+	pass # Replace with function body.
