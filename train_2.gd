e28f119f28fcef2d4f246e45444b677fd524ea05
JohnDaquaviousMichaelHoppkins <241607558+JohnDaquaviousMichaelHoppkins@users.noreply.github.com>
2026-02-28 19:31:53 +0200

    level 4

--- train_2.gd
+++ train_2.gd
@@ -20,6 +20,6 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	pass # Replace with function body.
 
 
-func _send_train1(send: Variant) -> void:
+func _send_train2(send: Variant) -> void:
 	do_move_train = true
 	pass
